f25b13953a91fc99d8f5616cdff8e904a1db5917
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2025-12-27 00:51:53 +0100

    added katalog-lib crate

---
+++ katalog-lib/src/lib.rs
@@ -0,0 +1,11 @@
+//! Common Library used by spel-katalog and arkiv-katalog.
+
+pub mod reflect {
+    //! Reflection module.
+
+    #[doc(inline)]
+    pub use ::katalog_lib_reflect::*;
+}
+
+#[doc(inline)]
+pub use ::katalog_lib_traits::*;
