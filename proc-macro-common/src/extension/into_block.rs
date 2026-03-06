04613b226e31c4177ca2ef4d00b1e6eb5dffdd91
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2026-03-06 11:50:22 +0100

    split up extension

---
+++ proc-macro-common/src/extension/into_block.rs
@@ -0,0 +1,50 @@
+//! [IntoBlock] impls.
+
+use ::proc_macro2::{Span, extra::DelimSpan};
+use ::syn::{Block, Stmt, StmtMacro, Token, token};
+
+use crate::extension::IntoStatement;
+
+/// Trait to convert interators of statements into blocks.
+pub trait IntoBlock {
+    /// Create a block using the given spans for delimiters and punctuation.
+    fn into_block(self, delim_span: DelimSpan, punct_span: impl FnMut() -> Span) -> Block;
+
+    /// Create a block with call site spans for delimiters and punctuation.
+    fn into_block_call_site(self) -> Block
+    where
+        Self: Sized,
+    {
+        self.into_block(token::Brace(Span::call_site()).span, Span::call_site)
+    }
+}
+
+impl<I> IntoBlock for I
+where
+    I: IntoIterator,
+    I::Item: IntoStatement,
+{
+    fn into_block(self, delim_span: DelimSpan, mut punct_span: impl FnMut() -> Span) -> Block {
+        let mut stmts = Vec::new();
+
+        for stmt in self {
+            if let Some(
+                Stmt::Expr(_, semi_token @ None)
+                | Stmt::Macro(StmtMacro {
+                    semi_token: semi_token @ None,
+                    ..
+                }),
+            ) = stmts.last_mut()
+            {
+                *semi_token = Some(Token![;](punct_span()))
+            }
+
+            stmts.push(stmt.into_statement());
+        }
+
+        Block {
+            brace_token: token::Brace { span: delim_span },
+            stmts,
+        }
+    }
+}
